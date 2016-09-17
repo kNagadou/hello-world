e1d6c0e17234751da3a44dc55ebca9cccd3eb515
kNagadou <ka.nagadou@gmail.com>
2016-09-18 02:12:02 +0900

    Add segmented controller

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,24 +12,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    let colors: Dictionary<String, UIColor> = ["White": UIColor.white, "Green": UIColor.green]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.rightButton.setTitle("Reset", for: UIControlState.normal)
-        self.leftButton.setTitle("Green", for: UIControlState.normal)
+        self.setTitleOfButton()
+        self.setTitleOfSegments()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func setTitleOfButton() {
+        self.rightButton.setTitle("White", for: UIControlState.normal)
+        self.leftButton.setTitle("Green", for: UIControlState.normal)
+    }
+    
+    func setTitleOfSegments() {
+        let numOfSegments: Int = self.segmentedControl.numberOfSegments
+        let colors: [String] = Array(self.colors.keys)
+        for i in 0..<numOfSegments {
+            self.segmentedControl.setTitle(colors[i], forSegmentAt: i)
+        }
+    }
+    
+    @IBAction func changeValue(_ sender: AnyObject) {
+        let ui: UISegmentedControl = sender as! UISegmentedControl
+        let segmentTitle: String? = ui.titleForSegment(at: ui.selectedSegmentIndex)
+        self.view.backgroundColor = self.colors[segmentTitle!]
+        log?.debug(segmentTitle!)
+    }
 
     @IBAction func onRightButton(_ sender: UIButton) {
-        self.view.backgroundColor = UIColor.white
+        self.view.backgroundColor = self.colors[sender.titleLabel!.text!]
         log?.debug(sender.titleLabel!.text!)
     }
 
     @IBAction func onLeftButton(_ sender: UIButton) {
-        self.view.backgroundColor = UIColor.green
+        self.view.backgroundColor = self.colors[sender.titleLabel!.text!]
         log?.debug(sender.titleLabel!.text!)
     }
 }
